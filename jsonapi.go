5c5bc7ece16ce57b967c7104eeabe5a1be9a82db
Hugo Landau <hlandau@devever.net>
2014-10-17 17:17:49 +0100

    Tweak necessary for use of btcjson with Namecoin.

--- jsonapi.go
+++ jsonapi.go
@@ -706,7 +706,8 @@ func rpcRawCommand(user string, password string, server string,
 		return result, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return nil, BadStatusCode(resp.StatusCode)
+	// -hlandau
+  //	return nil, BadStatusCode(resp.StatusCode)
 	}
 	result, err = GetRaw(resp.Body)
 	if err != nil {
